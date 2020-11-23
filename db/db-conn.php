60625274fbd1edba6e07ad6408c22be712e20544
Viktor Larsson <viktor.larsson8@learnet.se>
2020-11-23 10:54:29 +0100

    Tränat på mySQL

---
+++ db/db-conn.php
@@ -0,0 +1,28 @@
+<?php
+/*
+* PHP version 7
+* @category   
+* @author     Viktor Larsson <viktor.larsson020@gmail.com>
+* @license    PHP CC
+*/
+
+$user = "viktor";
+$db = "viktor";
+$host = "localhost";
+$pass = "m5acwIiFYDgff8RV";
+?>
+
+<?php
+/*
+* PHP version 7
+* @category   
+* @author     Viktor Larsson <viktor.larsson020@gmail.com>
+* @license    PHP CC
+* Denna är för bilar tabellen
+*/
+
+$user = "bilar";
+$db = "bilar";
+$host = "localhost";
+$pass = "rys5zapD9tQYWu1r";
+?>
