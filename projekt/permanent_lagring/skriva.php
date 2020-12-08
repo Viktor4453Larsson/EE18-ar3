b49f8cb3f9be2ae842239a2dfb2e4371ec6b036b
Viktor Larsson <viktor.larsson8@learnet.se>
2020-12-08 15:00:25 +0100

    Fortsatt med MySQL projektet del 2

---
+++ projekt/permanent_lagring/skriva.php
@@ -0,0 +1,65 @@
+
+
+<?php
+/*
+* En enkel blogg
+* PHP version 7
+* @category   webbapplikation med databas
+* @author     Viktor Larsson <viktor.larsson020@gmail.com>
+* @license    PHP CC
+*/
+include "./projekt/permanent_lagring/sakerhet/conn.php";
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Länder och GPS kordinater</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="permanent_lagring.css">
+</head>
+<body>
+    <div class="kontainer">
+    <h1>Länder och GPS kordinater</h1>
+    <nav>
+    <ul class="nav nav-tabs">
+        <li class="nav-item"><a class="nav-link" href="./taemot.php">Läsa</a></li>
+        <li class="nav-item"><a class="nav-link " href="./lagra.php">Skriv ut informationen</a></li>
+        <li class="nav-item"><a class="nav-link active" href="./skriva.php">Skriva</a></li>
+        <li class="nav-item"><a class="nav-link" href="./sok.php">Sök</a></li>
+    </ul>
+</nav>   
+        <form action="#" method="POST">
+            <label>Ange GPS kordinater ex. 59,297326418,0522474 <input type="text" name="kordinater"></label>
+            <label>Ange kommentarer<textarea name="kommentar"></textarea></label>
+            <label>Ange Land ex. Spanien<textarea name="namn"></textarea></label>
+            <button>Spara</button>
+        </form>
+        <?php
+        $gps = filter_input(INPUT_POST, "kordinater", FILTER_SANITIZE_STRING);
+        $land = filter_input(INPUT_POST, "namn", FILTER_SANITIZE_STRING);
+        $kommentar = filter_input(INPUT_POST, "kommentar", FILTER_SANITIZE_STRING);
+        if ($gps && $land && $kommentar) {
+            var_dump($gps, $land, $kommentar);
+            /* SQL anslutning */
+            $sql = "INSERT INTO lander (namn, kordinater, kommentar) VALUES ('$land', '$gps', '$kommentar')";
+
+            // Steg 2: Nu kör vi alla SQL satserna
+            $resultat = $conn->query($sql);
+
+            // Gick det att köra SQL koden?
+            if (!$resultat) {
+                die("Något blev fel med SQL-satsen");
+            } else {
+                echo "<p>Inlägget har registrerats</p>";
+            }
+
+            
+            // Steg 3: Stänga ner anslutningen
+            $conn->close();
+        }
+        ?>
+    </div>
+</body>
+</html>
--- projekt/permanent_lagring/taemot.php
+++ projekt/permanent_lagring/taemot.php
@@ -5,52 +5,61 @@
 * @author     Viktor Larsson <viktor.larsson020@gmail.com>
 * @license    PHP CC
 */
-include "./projekt/permanent_lagring/gps_kordinater.php";
-include "./projekt/permanent_lagring/lander.php";
+/* Vilken databas länkar vi till? */
+include "./projekt/permanent_lagring/sakerhet/conn.php";
 ?>
+<!-- Kropp och huvud -->
 <!DOCTYPE html>
 <html lang="sv">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Boostrap länk -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- Titeln på vår sida -->
     <title>Länder och GPS kordinater</title>
-    <link rel="stylesheet" href="permanent.css">
+    <!-- Bredd, läng och färger -->
+    <link rel="stylesheet" href="permanent_lagring.css">
 </head>
 <body>
+    <!-- En byrålåda för allting -->
     <div class="kontainer">
-    <h1>Länder och GPS kordinater</h1>
-    <ul class="nav nav-tabs">
+        <h1>Länder och GPS kordinater</h1>
+        <nav>
+            <ul class="nav nav-tabs">
                 <li class="nav-item"><a class="nav-link active" href="./taemot.php">Läs informationen som kommer in</a></li>
                 <li class="nav-item"><a class="nav-link " href="./lagra.php">Skriv ut informationen</a></li>
+                <li class="nav-item"><a class="nav-link " href="./skriva.php">Skriva</a></li>
                 <li class="nav-item"><a class="nav-link" href="./sok.php">Sök efter kordinater och länder</a></li>
             </ul>
-            <?php
-            $sql = "SELECT * FROM `gps`";
-            $sql1 = "SELECT * FROM `lander`";
-            $resultat = $conn->query($sql && $sql1);
+        </nav>
+        <?php
+        include "./projekt/permanent_lagring/sakerhet/conn.php";
+        /* Hämta alla tabellerna som vi har skapat och vill använda */
+        $sql = "SELECT * FROM lander";
+        $resultat = $conn->query($sql);
 
-            if (!$resultat) {
-                die("Det gick inte att ansluta till MySQL på någon av dina databaser!" . $conn->error);
-            } else {
-               echo "<p>Grattis det gick att ansluta, fortsätt bara</p>"; 
-            }
+        /* En kontrollcheck på att allt verkligen fungerar */
+        if (!$resultat) {
+            die("Det gick inte att ansluta till MySQL på någon av dina databaser!" . $conn->error);
+        } else {
+            echo "<p>Grattis det gick att ansluta, fortsätt bara</p>";
+        }
 
-            while ($rad = $resultat->fetch_assoc()) {
-                echo "<div class=\"inlägg\">";
-                echo "<h5>$rad[postText]</h5>";
-                echo "<h6>$rad[gps]</h6>";
-                echo "</div>";
+        /* Skriv ut alla den information som behöver skrivas ut */
+        while ($rad = $resultat->fetch_assoc()) {
+            echo "<div class=\"inlägg\">";
+                echo "<h5>$rad[id]</h5>";
+                echo "<h5>$rad[namn]</h5>";
+                echo "<h5>$rad[kordinater]</h5>";
+                echo "<h5>$rad[kommentar]</h5>";
+                echo "<h6>$rad[date]</h6>";
+                echo "</div>";  
+        }
+        /* Stäng av databasen, när vi inte behöver den längre */
+        $conn->close();
 
-                echo "<div class=\"inlägg\">";
-                echo "<h6>$rad[lander]</h6>";
-                echo "<p>$rad[date]</p>";
-                echo "</div>";
-                
-            }
-            $conn->close();
-            
-            ?>
+        ?>
     </div>
 </body>
 </html>
