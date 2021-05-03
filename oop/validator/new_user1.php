acbebbca2c0ddaddd89b012fec30a634f10fe3e9
Viktor4453Larsson <viktor.larsson@elev.ga.ntig.se>
2021-05-03 12:06:26 +0200

    OOP + Canvas

--- oop/validator/new_user1.php
+++ oop/validator/new_user1.php
@@ -5,13 +5,17 @@
 * @author     Viktor Larsson <viktor.larsson020@gmail.com>
 * @license    PHP CC
 */
-
 /* Ta emot data som skickas */
+function showErrors($type) {
+    echo "<p>";
+    foreach ($errors[$type] as $error) {
+        echo $error;
+    } 
+    echo "</p>";
+}
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING);
-
-
 /* Kontrollera om data finns */
 if ($username && $password && $email) {
     /* Kontrollera att alla variabler följer reglerna */
@@ -19,8 +23,6 @@ if ($username && $password && $email) {
     validatePassword($password);
     validateEmail($email);   
 }
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,15 +38,15 @@ if ($username && $password && $email) {
         <form action="#" method="post">
             <label>Username <input type="text" name="username" required></label>
             <?php
-            echo $resultat;
+            showErrors("username");
             ?>
             <label>Password <input type="password" name="password" required></label>
             <?php
-            echo $resultat;
+            showErrors("password");
             ?>
             <label>Email <input type="email" name="email" required></label>
             <?php
-            echo $resultat;
+            showErrors("email");
             ?>
             <button>Submit</button>
         </form>
@@ -53,43 +55,41 @@ if ($username && $password && $email) {
 </html>
 <?php
 /* Användarnamnet måste vara 6-12 tecken långt, stora & små bokstäver samt siffror */
-
-$resultat = "";
-
+$errors = [];
 function validateUsername($data)
 {
     // Hitta en bokstav i alfabetet
-    global $resultat;
+    global $errors;
     if (!preg_match("/[a-zA-Z0-9]{6,12}/", $data)) {
-        $resultat = "<p>&#10005; Innehåller INTE a-z, A_Z, 0-9 </p>";
+        $errors["username"][] = "<p>&#10005; Innehåller INTE a-z, A_Z, 0-9 </p> <br>";
     }
 }
 function validatePassword($data)
 {
     // Hitta en bokstav i alfabetet
-    global $resultat;
+    global $errors;
     if (!preg_match("/[a-zåäö]/", $data) > 0) {
-        $resultat = 'password must contain a least one lowercase character';
+        $errors["password"][] = 'password must contain a least one lowercase character <br>';
     }
     if (!preg_match("/[A-ZÅÄÖ]/", $data) > 0) {
-        $resultat = 'password must contain a least one uppercase character';
+        $errors["password"][] = 'password must contain a least one uppercase character <br>';
     }
     if (!preg_match("/[0-9]/", $data) > 0) {
-        $resultat = 'password must contain a least one alphanumeric';
+        $errors["password"][] = 'password must contain a least one alphanumeric <br>';
     }
     if (!preg_match("/[#%&¤_\*\-\+\@\!\?\(\)\[\]\$£€]/", $data) > 0) {
-        $resultat = 'password must contain a least one special character';
+        $errors["password"][] = 'password must contain a least one special character <br>';
     }
     if (!preg_match("/^.{8,40}$/", $data) > 0) {
-        $resultat = 'password must at least 8 character long';
+        $errors["password"][] = 'password must at least 8 character long <br>';
     }
 }
 function validateEmail()
 {
     // Hitta en bokstav i alfabetet
-    global $resultat;
+    global $errors;
     if (!filter_var($data, FILTER_VALIDATE_EMAIL)) {
-        $resultat = "Invalid email format";
+        $errors["email"][] = "Invalid email format";
       }
 }
 ?>
